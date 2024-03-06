504e53d9ba122ecc5e280d48008451a62c8b72db
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-03-06 01:07:26 -0600

    add(service): EmailService
    Implementation of IEmailService in this class

---
+++ src/Controllers/EmailService.cs
@@ -0,0 +1,61 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Util.Store;
+using SMTPSender.src.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SMTPSender.src.Controllers
+{
+    public class EmailService : IEmailService
+    {
+        private readonly static EmailService _service = new EmailService();
+        private const int PORT = 587;
+        private const string HOST = "smtp.gmail.com";
+        private string[] AuthPermissions = { GmailService.Scope.GmailSend };
+        private readonly string ApplicationName = "Example Desktop client";
+        private const string CREDENTIALS_FILENAME = "credentials.json";
+        private string basePath = Path.Combine(Directory.GetCurrentDirectory(), CREDENTIALS_FILENAME);
+        private string tokenPath = Path.Combine(Directory.GetCurrentDirectory(), "token.json");
+
+        private UserCredential credential;
+        private SmtpClient smtpClient;
+        private MailMessage message;
+
+        public static EmailService Service { get { return _service; } }
+
+        public EmailService() { }
+
+        public async Task<bool> Authenticate()
+        {
+            using(var stream = new FileStream(basePath, FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    AuthPermissions,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(tokenPath, true)
+                ); 
+            }
+
+            if (credential is null)
+            {
+                return await Task.FromResult(false);
+            }
+
+            return await Task.FromResult(true);
+        }
+
+        public void SendEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
