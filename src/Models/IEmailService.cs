4294b1cf2a60071bb978a640982f4a3e1fde60bd
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-03-06 01:05:54 -0600

    add(interface): generate EmailService Interface

---
+++ src/Models/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMTPSender.src.Models
+{
+    public interface IEmailService
+    {
+        void SendEmail(string email);
+
+        Task<bool> Authenticate();
+    }
+}
